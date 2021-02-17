5f1d58ea2a7e97aeab305bf0784ba4760a22603b
Nevin Herring <nevin.herring@gmail.com>
2021-02-16 22:33:28 -0600

    checking c# rounding rules

--- Program.cs
+++ Program.cs
@@ -25,9 +25,18 @@ namespace CastingConverting
             int h = ToInt32(g);
             WriteLine($"g is {g} and h is {h}");
             double[] doubles = new[] { 9.49, 9.5, 9.51, 10.49, 10.5, 10.51 };
+            // foreach (double n in doubles)
+            // {
+            //     WriteLine($"ToInt({n}) is {ToInt32(n)}");
+            // }
             foreach (double n in doubles)
             {
-                WriteLine($"ToInt({n}) is {ToInt32(n)}");
+                WriteLine(format:
+                    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+                    arg0: n,
+                    arg1: Math.Round(value: n, digits: 0,
+                    mode: MidpointRounding.AwayFromZero)
+                );
             }
         }
     }
